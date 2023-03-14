2d38cb96e40d6d6fa9b4ae6bb43f67b132a48e5c
bamboo-nova <suzuk1nov11@gmail.com>
2023-03-15 00:31:46 +0900

    in progress day3morning

---
+++ day3morning/src/Traits/important_traits/README.md
@@ -0,0 +1,8 @@
+We will now look at some of the most common traits of the Rust standard library:
+
+- Iterator and IntoIterator used in for loops,
+- From and Into used to convert values,
+- Read and Write used for IO,
+- Add, Mul, … used for operator overloading, and
+- Drop used for defining destructors.
+- Default used to construct a default instance of a type.
--- day3morning/src/Traits/traits.rs
+++ day3morning/src/Traits/traits.rs
@@ -1,4 +1,6 @@
 // 型を抽象化するためにtraitを用いる。Goのインターフェースと仕組みは類似している
+// Traitは予め実装されているメソッドとユーザーが自分で実装することが要求されるメソッドが指定できる
+// デフォルトの実装をもつメソッドは実際のメソッドに依存できる
 
 trait Greet {
     fn say_hello(&self);
@@ -23,6 +25,8 @@ impl Greet for Cat {
 }
 
 fn main() {
+    // 下記のように実装することはできるが、traitsを使って抽象化して保証しない場合、
+    // Vec<Greet>を持つことができない
     let pets: Vec<Box<dyn Greet>> = vec![
         Box::new(Dog { name: String::from("Fido") }),
         Box::new(Cat),
@@ -30,4 +34,12 @@ fn main() {
     for pet in pets {
         pet.say_hello();
     }
+    println!("{} {}", std::mem::size_of::<Dog>(), std::mem::size_of::<Cat>());
+    println!("{} {}", std::mem::size_of::<&Dog>(), std::mem::size_of::<&Cat>());
+    // dyn Greetは実装した動的な大きさの型についてコンパイラに伝えるための方法(8+8=16)
+    // petsはGreetを実装したオブジェクトへのFat pointerを持っている
+    // Fat Pointerは実際のオブジェクトへのポインタとその特定のオブジェクトのGreet実装のための
+    // 仮想メソッド・テーブルへのポインタの二つで構成されている
+    println!("{}", std::mem::size_of::<&dyn Greet>());
+    println!("{}", std::mem::size_of::<Box<dyn Greet>>());
 }
